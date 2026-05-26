747ff90c1bff9c0cc0c595e7c15391c29f0a4c4d
Serdan1 <danielserranoreal@gmail.com>
2026-05-26 13:55:25 +0200

    definimos los datos de una variable

--- src/Symbol.java
+++ src/Symbol.java
@@ -1,3 +1,12 @@
 public class Symbol {
 
+    String name;
+    String type;
+    boolean initialized;
+
+    public Symbol(String name, String type, boolean initialized) {
+        this.name = name;
+        this.type = type;
+        this.initialized = initialized;
+    }
 }
