5865d5545c8731ba7d584cbb09df4ce8256724a1
Serdan1 <danielserranoreal@gmail.com>
2026-05-26 13:56:12 +0200

    almacenamos y gestionamos las variables

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -1,3 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SymbolTable {
 
+    private Map<String, Symbol> table = new HashMap<>();
+
+    public void declare(String name, String type) {
+        table.put(name, new Symbol(name, type, false));
+    }
+
+    public boolean exists(String name) {
+        return table.containsKey(name);
+    }
+
+    public void initialize(String name) {
+        if (exists(name)) {
+            table.get(name).initialized = true;
+        }
+    }
+
+    public boolean isInitialized(String name) {
+        return exists(name) && table.get(name).initialized;
+    }
 }
